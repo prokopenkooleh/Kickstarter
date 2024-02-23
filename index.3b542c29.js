57f738b64fea193e30c8e594ccd64302cd90fa6d
Oleh Prokopenko <olehprokopenko1@gmail.com>
2024-02-23 22:24:30 +0200

    make build

---
+++ index.3b542c29.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-undef, no-unused-vars
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",type:"fraction"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});//# sourceMappingURL=index.3b542c29.js.map
+
+//# sourceMappingURL=index.3b542c29.js.map
